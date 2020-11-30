93f61fc32b766a3c6eafbbb3f92c3a28229cabc4
UriKurae <youtfrost@gmail.com>
2020-11-30 22:37:59 +0100

    Scene 1 and 2

--- Game/Source/Scene2.cpp
+++ Game/Source/Scene2.cpp
@@ -4,20 +4,20 @@
 #include "Render.h"
 #include "Textures.h"
 #include "Map.h"
-#include "Scene1.h"
+#include "Scene2.h"
 #include "Player.h"
 #include "FadeToBlack.h"
 
-Scene1::Scene1()
+Scene2::Scene1()
 {
-	name.Create("scene1");
+	name.Create("scene2");
 }
 
-Scene1::~Scene1()
+Scene2::~Scene1()
 {
 }
 
-bool Scene1::Start()
+bool Scene2::Start()
 {
 	sky = app->tex->Load("Assets/textures/sky.png");
 	sea = app->tex->Load("Assets/textures/sea.png");
@@ -27,7 +27,7 @@ bool Scene1::Start()
 	return true;
 }
 
-bool Scene1::Update(float dt)
+bool Scene2::Update(float dt)
 {
 	if (app->player->godMode == false)
 	{
@@ -41,20 +41,20 @@ bool Scene1::Update(float dt)
 	return true;
 }
 
-bool Scene1::Draw()
+bool Scene2::Draw()
 {
 	bool ret = true;
-	
-	if(app->input->GetKey(SDL_SCANCODE_ESCAPE) == KEY_DOWN)
+
+	if (app->input->GetKey(SDL_SCANCODE_ESCAPE) == KEY_DOWN)
 		ret = false;
-	
+
 	app->render->DrawTexture(sky, -200, -10, NULL, 0.65f);
 	app->render->DrawTexture(clouds, -200, 180, NULL, 0.75f);
 	app->render->DrawTexture(sea, -200, 395, NULL, 0.85f);
-	
-	if(app->map->active == true)
+
+	if (app->map->active == true)
 		app->map->Draw();
-	
+
 	return ret;
 
 	//if (app->input->GetKey(SDL_SCANCODE_T) == KEY_DOWN)
@@ -73,49 +73,49 @@ bool Scene1::Draw()
 	//app->pathFinding->DrawPath();
 }
 
-bool Scene1::CleanUp()
+bool Scene2::CleanUp()
 {
 	LOG("Freeing scene");
 	app->tex->UnLoad(sky);
 	app->tex->UnLoad(clouds);
 	app->tex->UnLoad(sea);
-	
+
 	return true;
 }
 
-bool Scene1::RestartPlayerPosition()
+bool Scene2::RestartPlayerPosition()
 {
 	app->player->SetPosition(playerStartPosition.x, playerStartPosition.y);
 
 	return true;
 }
 
-int Scene1::CheckWin()
+int Scene2::CheckWin()
 {
 	ListItem<MapLayer*>* layer = app->map->data.layers.start;
-	
-	iPoint playerPosTop = app->map->WorldToMap(app->player->GetPosition().x+8, app->player->GetPosition().y+15);
-	
+
+	iPoint playerPosTop = app->map->WorldToMap(app->player->GetPosition().x + 8, app->player->GetPosition().y + 15);
+
 	while (layer != NULL)
 	{
-	
+
 		if (layer->data->name == "HitBoxes")
 		{
 			uint playerMidTile = layer->data->Get(playerPosTop.x, playerPosTop.y);
-	
+
 			if (playerMidTile == 1166)
 			{
 				return 1;
 			}
-	
+
 			if (playerMidTile == 1170)
 			{
 				return 2;
 			}
 		}
-	
+
 		layer = layer->next;
 	}
-	
+
 	return -1;
 }
---
+++ Game/Source/Scene2.h
@@ -0,0 +1,40 @@
+#pragma once
+
+#include "Scene.h"
+
+
+class Scene2 : public Scene
+{
+	Scene2();
+
+	// Destructor
+	virtual ~Scene2();
+
+	// Called before the first frame
+	bool Start();
+
+	// Called each loop iteration
+	bool Update(float dt);
+
+	// Draw Method
+	bool Draw();
+
+	// Called before quitting
+	bool CleanUp();
+
+	// Restart level
+	bool RestartPlayerPosition();
+
+	// Check if player won
+	int CheckWin();
+
+private:
+
+	// Player's start position as iPoint  
+	iPoint playerStartPosition;
+
+	// Textures of the Scene2
+	SDL_Texture* sky;
+	SDL_Texture* clouds;
+	SDL_Texture* sea;
+};
